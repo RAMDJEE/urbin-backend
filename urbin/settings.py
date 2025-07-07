650933421cc1631e87146aeb7ac8fef7ddd4de7c
Hassane Ramdjee <hassaneramdjee.nirvana@gmail.com>
2025-07-07 11:18:34 +0200

    j'en ai marre

--- urbin/settings.py
+++ urbin/settings.py
@@ -77,10 +77,10 @@ WSGI_APPLICATION = "urbin.wsgi.application"
 
 DATABASES = {
     'default': dj_database_url.config(
-        default=os.environ.get("DATABASE_URL")
+        default="sqlite:///db.sqlite3",  # fallback local si DATABASE_URL absent
+        conn_max_age=600,
     )
 }
-
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
 
