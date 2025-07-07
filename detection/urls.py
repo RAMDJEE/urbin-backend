63bcf0ea2e7df12e6d5310fb806fb8d25e8c6c07
Hassane Ramdjee <hassaneramdjee.nirvana@gmail.com>
2025-07-07 15:38:29 +0200

    Force Dockerfile to run entrypoint.sh

--- detection/urls.py
+++ detection/urls.py
@@ -4,13 +4,10 @@ from . import views
 from .views import hello_world, register_user, login_user
 
 urlpatterns = [
-    path('upload/', views.upload_image, name='upload_image'),
-    path('success/', lambda r: render(r, 'success.html'), name='image_success'),
-    path('register/', views.register_view, name='register'),
+    path('api/login/', views.login_user),
+    path('api/register/', views.register_user),
+    path('', views.upload_image, name='upload_image'),
     path('login/', views.login_view, name='login'),
     path('logout/', views.logout_view, name='logout'),
-    path('api/hello/', hello_world),
-    path('api/register/', register_user),
-    path('api/login/', login_user),
-    path("api/bins/", views.bins_data, name="bins_data"),
+    path('register/', views.register_view, name='register'),
 ]
