48a2c47f0ee2bded2689d1faa1e0489304a0750e
Hassane Ramdjee <hassaneramdjee.nirvana@gmail.com>
2025-07-07 13:44:18 +0200

    Use entrypoint script to run migrate and start gunicorn

--- Dockerfile
+++ Dockerfile
@@ -14,5 +14,4 @@ RUN pip install -r requirements.txt
 
 EXPOSE 8080
 
-CMD bash -c "python manage.py migrate --noinput && python manage.py collectstatic --noinput && python import_csv.py && gunicorn urbin.wsgi:application --bind 0.0.0.0:8080"
-
+ENTRYPOINT ["./entrypoint.sh"]
---
+++ entrypoint.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+# Apply database migrations
+echo "Applying database migrations..."
+python manage.py migrate --noinput
+
+# Collect static files
+echo "Collecting static files..."
+python manage.py collectstatic --noinput
+
+# Optional: import your CSV
+echo "Importing CSV..."
+python import_csv.py
+
+# Start Gunicorn
+echo "Starting Gunicorn..."
+gunicorn urbin.wsgi:application --bind 0.0.0.0:8080
